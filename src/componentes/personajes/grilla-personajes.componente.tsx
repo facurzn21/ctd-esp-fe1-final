7379a49421d64b457385fd49346a3059bddda557
facurzn21 <facundoeze@gmail.com>
2024-03-30 03:48:37 -0300

    probando logica para grilla component

--- src/componentes/personajes/grilla-personajes.componente.tsx
+++ src/componentes/personajes/grilla-personajes.componente.tsx
@@ -1,21 +1,33 @@
-import './grilla-personajes.css';
-import TarjetaPersonaje from './tarjeta-personaje.componente';
+import { cargarPersonajes } from "../../actions/personajes.actions";
+import "./grilla-personajes.css";
+import TarjetaPersonaje from "./tarjeta-personaje.componente";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 
 /**
  * Grilla de personajes para la pagina de inicio
- * 
+ *
  * Deberás agregar las funciones necesarias para mostrar y paginar los personajes
- * 
- * 
- * @returns un JSX element 
+ *
+ *
+ * @returns un JSX element
  */
 const GrillaPersonajes = () => {
+  const dispatch = useDispatch();
+  // Obten los personajes desde el estado
+  const personajes = useSelector((state) => state.personajes.lista);
 
-    return <div className="grilla-personajes">
-       <TarjetaPersonaje />
-       <TarjetaPersonaje />
-       <TarjetaPersonaje />
+  useEffect(() => {
+    dispatch(cargarPersonajes());
+  }, [dispatch]);
+
+  return (
+    <div className="grilla-personajes">
+      <TarjetaPersonaje />
+      <TarjetaPersonaje />
+      <TarjetaPersonaje />
     </div>
-}
- 
+  );
+};
+
 export default GrillaPersonajes;
